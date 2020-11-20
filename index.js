00f6f87bc05939b0ee2a79933cacc6cc3f140041
Juu-yzx <72299655+Juu-yzx@users.noreply.github.com>
2020-11-20 13:21:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const axios = require("axios")
-const urls = ["https://boulder-sophisticated-swift.glitch.me"]
+const urls = ["https://glitch.com/edit/#!/join/307d5b02-81e9-4d38-8e40-cf9c684a8794"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
         })
     }, 60 * 1000);
+

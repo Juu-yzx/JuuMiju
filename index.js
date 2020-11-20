6368525a6dd7253a2753c341fb0bec1921ac134d
Juu-yzx <72299655+Juu-yzx@users.noreply.github.com>
2020-11-20 12:54:39 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const axios = require("axios")
+const urls = ["Link"]
+setInterval(function() {
+            urls.forEach(url => {
+            axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
+        })
+    }, 60 * 1000);
